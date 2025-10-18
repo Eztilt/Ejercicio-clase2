e668a1621c657089bae581b69029aa591c5de79a
Tom <tomascv2003@hotmail.com>
2025-10-18 12:28:50 -0500

    funcionalidad agregada

--- src/App.java
+++ src/App.java
@@ -1,5 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Prueba para la segunda clase");
+        System.out.println("Modificacion 1 para nueva rama");
+        System.out.println("Modificacion agregada para nueva rama");
     }
 }
